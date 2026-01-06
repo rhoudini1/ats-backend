e4acb7a165cefee809ce66a3a2faa732e00b1f29
Renan Monteiro <araujomonteirogba@hotmail.com>
2026-01-06 15:53:29 -0300

    chore: add Scalar api reference

--- src/ATS.API/Program.cs
+++ src/ATS.API/Program.cs
@@ -1,9 +1,9 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +12,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
