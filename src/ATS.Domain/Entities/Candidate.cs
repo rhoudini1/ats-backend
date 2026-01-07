e9e277172653e0a06a38df7a996b6b5e3a79ea2e
Renan Monteiro <araujomonteirogba@hotmail.com>
2026-01-07 08:05:01 -0300

    feat: candidate domain entity

---
+++ src/ATS.Domain/Entities/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using ATS.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATS.Domain.Entities;
+
+public class Candidate
+{
+    public Guid Id { get; init; }
+
+    public required string FullName { get; set; }
+
+    public required Email Email { get; set; }
+
+    public string? ResumeUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+}
