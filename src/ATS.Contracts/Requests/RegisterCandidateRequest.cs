84b0b4fb09c4378e5d2904cd06159c1ea8bda3e0
Renan Monteiro <araujomonteirogba@hotmail.com>
2026-01-07 12:15:09 -0300

    feat: define contracts for register candidate request and response

---
+++ src/ATS.Contracts/Requests/RegisterCandidateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATS.Contracts.Requests;
+
+public class RegisterCandidateRequest
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
---
+++ src/ATS.Contracts/Responses/RegisterCandidateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATS.Contracts.Responses;
+
+public class RegisterCandidateResponse
+{
+    public required Guid Id { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+}
